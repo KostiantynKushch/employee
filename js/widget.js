c85dc83140b65f2603bee5970d3b7feeb9901b50
kostiantynkushch <kostiantyn.kushch@gmail.com>
2020-06-08 15:54:11 +0300

    init

---
+++ js/widget.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const url = '../employee/data/employees.json';
+const liTmpl = document.querySelector('[data-liTmpl]').innerHTML;
+const empList = document.querySelector('[data-empList]');
+
+
+window.addEventListener('load', getData);
+
+function getData() {
+	fetch(url)
+		.then(result => result.json())
+		.then(showEmployeeList);
+}
+
+function showEmployeeList(data) {
+
+	data.forEach(emp => {
+		empList.innerHTML += liTmpl.replace(/{{name}}/gi, emp.name)
+			.replace(/{{status}}/gi, emp.inoffice ? 'in' : 'out');
+	});
+}
